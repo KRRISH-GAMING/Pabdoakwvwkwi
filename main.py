588c59801cf63471cff3f32725f1d5130c5ce7a0
KRRISH-GAMING <79586177+KRRISH-GAMING@users.noreply.github.com>
2025-09-29 20:37:47 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -170,14 +170,14 @@ async def restart_bots():
                 set_client(bot_me.id, xd)
                 print(f"✅ Restarted clone bot @{bot_me.username} ({bot_me.id})")
 
-            fresh = await db.get_clone_by_id(bot_me.id)
+            """fresh = await db.get_clone_by_id(bot_me.id)
             if fresh and fresh.get("auto_post", False):
                 auto_post_channel = fresh.get("ap_channel", None)
                 if auto_post_channel:
                     asyncio.create_task(
                         auto_post_clone(bot_me.id, db, auto_post_channel)
                     )
-                    print(f"▶️ Auto-post started for @{bot_me.username}")
+                    print(f"▶️ Auto-post started for @{bot_me.username}")"""
         except (UserDeactivated, AuthKeyUnregistered):
             print(f"⚠️ Bot {bot_id} invalid/deactivated. Removing from DB...")
             await db.delete_clone_by_id(bot_id)
