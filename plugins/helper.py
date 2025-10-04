1563445276e20840247ad9f0555dc2e7d8e9a668
KRRISH-GAMING <79586177+KRRISH-GAMING@users.noreply.github.com>
2025-10-04 21:16:26 +0530

    Add files via upload

--- plugins/helper.py
+++ plugins/helper.py
@@ -28,7 +28,7 @@ async def safe_action(coro_func, *args, **kwargs):
             print(f"⚠️ User blocked the bot. Skipping reply...")
             return
         except Exception as e:
-            if "MESSAGE_NOT_MODIFIED" not in str(e) and "MESSAGE_ID_INVALID" not in str(e):
+            if "MESSAGE_NOT_MODIFIED" not in str(e) and "MESSAGE_ID_INVALID" not in str(e) and "QUERY_ID_INVALID" not in str(e):
                 raise
             if "INPUT_USER_DEACTIVATED" in str(e):
                 print(f"⚠️ User {user_id} account is deleted. Skipping batch...")
