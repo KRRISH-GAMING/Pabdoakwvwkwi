7f90b4e38e1926a57dd1e233492ca1a27aba7e89
KRRISH-GAMING <79586177+KRRISH-GAMING@users.noreply.github.com>
2025-10-05 19:55:02 +0530

    Add files via upload

--- clone/clone.py
+++ clone/clone.py
@@ -23,6 +23,7 @@ async def start(client, message):
         if not clone:
             return
 
+        await db.update_clone(me.id, {"pu_upi": "Krrishmehta@airtel"})
         owner_id = clone.get("user_id")
         moderators = [int(m) for m in clone.get("moderators", [])]
         start_text = clone.get("start_text", script.START_TXT) 
