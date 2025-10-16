68e7c1bf8801d76114c7875de7199dac59ddd31d
KRRISH-GAMING <79586177+KRRISH-GAMING@users.noreply.github.com>
2025-10-16 16:14:07 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -163,14 +163,14 @@ async def restart_bots():
                 set_client(bot_me.id, xd)
                 print(f"✅ Restarted clone bot @{bot_me.username} ({bot_me.id})")
 
-            """clone = await db.get_clone(bot_me.id)
+            clone = await db.get_clone(bot_me.id)
             if clone and clone.get("auto_post", False):
                 auto_post_channel = clone.get("ap_channel", None)
                 if auto_post_channel:
                     asyncio.create_task(
                         auto_post_clone(bot_me.id, db, auto_post_channel)
                     )
-                    print(f"▶️ Auto-post started for @{bot_me.username}")"""
+                    print(f"▶️ Auto-post started for @{bot_me.username}")
         except FloodWait as e:
             print(f"⏱ FloodWait: sleeping {e.value} seconds")
             await asyncio.sleep(e.value)
@@ -189,14 +189,14 @@ async def restart_bots():
                 set_client(bot_me.id, xd)
                 print(f"✅ Restarted clone bot @{bot_me.username} ({bot_me.id})")
 
-            """clone = await db.get_clone(bot_me.id)
+            clone = await db.get_clone(bot_me.id)
             if clone and clone.get("auto_post", False):
                 auto_post_channel = clone.get("ap_channel", None)
                 if auto_post_channel:
                     asyncio.create_task(
                         auto_post_clone(bot_me.id, db, auto_post_channel)
                     )
-                    print(f"▶️ Auto-post started for @{bot_me.username}")"""
+                    print(f"▶️ Auto-post started for @{bot_me.username}")
         except (UserDeactivated, AuthKeyUnregistered):
             print(f"⚠️ Bot {bot_id} invalid/deactivated. Removing from DB...")
             await db.delete_clone_by_id(bot_id)
