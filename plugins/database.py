17fd6034bd004629d6bb42107b6f6ba42cbfb090
KRRISH-GAMING <79586177+KRRISH-GAMING@users.noreply.github.com>
2025-10-18 00:13:12 +0530

    Add files via upload

--- plugins/database.py
+++ plugins/database.py
@@ -1,4 +1,4 @@
-import motor.motor_asyncio, time as pytime, 
+import motor.motor_asyncio, time as pytime
 from datetime import datetime, timedelta
 from bson import ObjectId
 from plugins.config import *
