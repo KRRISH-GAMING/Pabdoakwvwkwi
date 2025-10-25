259953ed92121736c199d7e0c8ff5416649acdaa
KRRISH-GAMING <79586177+KRRISH-GAMING@users.noreply.github.com>
2025-10-25 18:36:48 +0530

    Add files via upload

--- plugins/config.py
+++ plugins/config.py
@@ -20,7 +20,7 @@ try:
     # Bot Information
     API_ID = int(os.environ.get("API_ID", "15479023"))
     API_HASH = os.environ.get("API_HASH", "f8f6cf547822449c29fc60dae3b31dd4")
-    SESSION_STRING = os.environ.get("SESSION_STRING", "BQDsMO8AmFb6JbgFyK7jiJtXcx3AFBuboExTZHINbxsl8_YzR0HaeAI5_BnsfUv_vN-vrB8NvarvyBvTRb80QQsTUuCahomUwfyd4lYuGyiQ3olZsxvJ-jKg_5XvfMN6DalcD2zNuWGf-FvvTeH_-t8QMcAPXpDxyt97bYsBIBtQAoTDpHu5bqf0h6XphvYAnYPBWLluo6VASKQJ2FsxPQfV0pEflImcLKiakUFNzA5Sn0AX6ZzRbP9gmGvKJg5L4aOD7SmYwaDhm6N7xR4p8jtpx4zszlxriOQB_lCjywawyWw-_O01f0roGKph7TGLkSEr_uJ0asKkJAyIQ3yDiJ751R51JwAAAABaJgrVAA")
+    SESSION_STRING = os.environ.get("SESSION_STRING", "BQDsMO8AYBThDSMHkoXplgI2_wYTP-7m6WX79Yn9ihzDp0Ika1b025mWfR9gYs0-BOukentqjnsb1izUT4Mg_5HHnbqOB_ZLPvgNaMAGRCCgM2HYPxB531pGoZvNh2gpIqE2m4OE7DGreCjyy_R_wCOrkSh559u7T1rSYAQrJ1N_TQ7hRNe55gPMweG0eEJgUFvrf2pkP_iWF8ifxncWHr1u8av868qPUaYyUxdGQPo7_ChsdKq8unSj_UU4xakuNx8X6GQTStF1Uc3iM4Yb1VDWfojj_ByVPmnT8XIE8qQJLVV-gy8uN96Uhryt-nMPm2HDF1Hnzq60SZiZd-FsHeh2epOhZQAAAABaJgrVAA")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "8136275296:AAEjRpfg_ir7uGU4zyQoR5V55tvSZNPFH-4")
     BOT_USERNAME = os.environ.get("BOT_USERNAME", "KMCloneManagerBot") # without @
 
